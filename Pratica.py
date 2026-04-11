8b1cb70a258d72700de83c49f3785fcd90bf7cd8
Rafael Azevedo <rafael.azevedo@msn.com>
2026-04-10 21:33:12 -0300

    Add files via upload

---
+++ Pratica.py
@@ -0,0 +1,14 @@
+# ATIVIDADE ORIENTADA 1
+
+import pandas as pd
+import time
+arquivos = [
+"202501_NovoBolsaFamilia.csv", "202502_NovoBolsaFamilia.csv", "202503_NovoBolsaFamilia.csv", "202504_NovoBolsaFamilia.csv",
+"202505_NovoBolsaFamilia.csv", "202506_NovoBolsaFamilia.csv"
+]
+inicio = time.time()
+dfs = [pd.read_csv(f, encoding="LATIN1", sep=";", nrows=100) for f in arquivos]
+df = pd.concat(dfs)
+print(df.head())
+tempo = time.time() - inicio
+print(f"Pandas: {tempo:.2f}s")
